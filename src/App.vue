a5b5464534254b63146fbbfc51ba0fc2b84dddb4
Daniel Akpan <akpandaniel.n@gmail.com>
2023-01-01 17:29:54 +0100

    add: TodoHeader, theme modifier

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import { useDark } from "@vueuse/core";
 
 import PageHeader from "./components/PageHeader.vue";
+import TodoHeader from "./components/Todo/TodoHeader.vue";
 
 export default {
   data() {
@@ -11,6 +12,7 @@ export default {
   },
   components: {
     PageHeader,
+    TodoHeader,
   },
   mounted() {
     document.body.classList.add("bg-gray-200", "dark:bg-gray-900");
@@ -21,11 +23,16 @@ export default {
 <template>
   <main>
     <PageHeader :isDark="isDark" />
+    <div class="max-w-[600px] mx-auto pt-20">
+      <TodoHeader />
+    </div>
   </main>
 </template>
 
 <style>
-#todo-wrapper {
-  margin-inline: auto;
+@import url("https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@400;700&display=swap");
+main {
+  font-family: "Josefin Sans", sans-serif;
+  font-weight: 400;
 }
 </style>
---
+++ src/components/Todo/TodoHeader.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { useDark, useToggle } from "@vueuse/core";
+
+const isDark = useDark();
+const toggleDark = useToggle(isDark);
+</script>
+
+<template>
+  <div class="flex justify-between">
+    <h1 class="font-semibold text-white text-3xl tracking-[18px]">TODO</h1>
+    <button @click="toggleDark()">
+      <img
+        v-if="isDark"
+        src="/assets/images/icon-moon.svg"
+        alt="switch theme"
+      />
+      <img v-else src="/assets/images/icon-sun.svg" alt="switch theme" />
+    </button>
+  </div>
+</template>
