4f9c613d2a9152918abe8229db5125a1b1311a2e
Daniel Akpan <akpandaniel.n@gmail.com>
2023-01-01 17:12:31 +0100

    add page header

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,31 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+import { useDark } from "@vueuse/core";
+
+import PageHeader from "./components/PageHeader.vue";
+
+export default {
+  data() {
+    return {
+      isDark: useDark(),
+    };
+  },
+  components: {
+    PageHeader,
+  },
+  mounted() {
+    document.body.classList.add("bg-gray-200", "dark:bg-gray-900");
+  },
+};
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <PageHeader :isDark="isDark" />
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+<style>
+#todo-wrapper {
+  margin-inline: auto;
 }
 </style>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  props: {
+    isDark: Boolean,
+  },
+};
+</script>
+
+<template>
+  <section class="absolute h-[300px] -z-10">
+    <img
+      v-if="isDark"
+      class="w-full block h-full bg-no-repeat object-fit object-left"
+      src="/assets/images/bg-desktop-dark.jpg"
+      alt="header bg"
+    />
+    <img
+      v-else
+      class="w-full block h-full bg-no-repeat object-fit object-left"
+      src="/assets/images/bg-desktop-light.jpg"
+      alt="header bg"
+    />
+  </section>
+</template>
